ca004f0ceb90a821411ea06d74e1b1b389ff9e7d
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-06-23 15:22:48 +0530

    backend link uploaded

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mini-fullstack-w0i9.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
